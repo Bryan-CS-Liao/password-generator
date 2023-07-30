7893266ee913667c5652a6338df733d34614a485
Bryan-CS-Liao <bryprocs@gmail.com>
2023-07-30 16:44:28 -0700

    simple and slightly more complicated versions

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+password = ""
+for char in range(1, nr_letters + 1):
+    randomChar = random.choice(letters)
+    password += randomChar
+for sym in range(1, nr_symbols + 1):
+    randomSym = random.choice(symbols)
+    password += randomSym
+for num in range(1, nr_numbers + 1):
+    randomNum = random.choice(numbers)
+    password += randomNum
+print(password)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+passwordList = []
+for char in range(1, nr_letters + 1):
+    passwordList += random.choice(letters)
+
+for sym in range(1, nr_symbols + 1):
+    passwordList += random.choice(symbols)
+
+for num in range(1, nr_numbers + 1):
+    passwordList += random.choice(numbers)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+random.shuffle(passwordList)
+#print(passwordList)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+password1 = ""
+for char in passwordList:
+    password1 += char
+print(password1)
